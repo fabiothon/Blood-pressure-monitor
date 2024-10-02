840dc4d0122b91fa664756a456feba2fb3c5155c
Fabio Thon <116906028+fabiothon@users.noreply.github.com>
2024-10-02 16:08:27 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Blood pressure as a function of time
+
+This R script visualizes and analyzes blood pressure data using `ggplot2`, `dplyr`, `readxl`, and `ggrepel`. It imports an Excel file containing timestamps, systolic and diastolic pressure, and heart rate (pulse) readings. The timestamps are converted to POSIXct format for accurate time plotting.
+
+The script generates a line plot, where systolic, diastolic pressure, and pulse are displayed over time with distinct colors (red for systolic, blue for diastolic, and grey for pulse). The plot includes a secondary y-axis for heart rate and is annotated with non-overlapping labels using `geom_text_repel` to display individual data values.
+
+The plot is further customized with axis labels, a title, a caption, and a dynamic subtitle showing average systolic and diastolic pressure. The mean systolic and diastolic pressures are calculated at the end of the script, rounding the systolic mean to one decimal place for clarity.
+
+## Requirements
+- [R](https://www.r-project.org)
+
+## Usage
+
+1. Download of all necessary files
+2. Install necessary libraries on your local environment
+3. Run application
+
+## Contributing
+
+Pull requests are welcome! For major changes, please open an issue first
+to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## Licenses
+This script uses the [MIT](https://choosealicense.com/licenses/mit/) License.
---
+++ main.R
@@ -0,0 +1,58 @@
+# Libraries -----
+library(ggplot2)
+library(dplyr)
+library(readxl)
+library(ggrepel)
+
+# Global setting:
+options(ggrepel.max.overlaps = Inf)
+
+# Excel file input ------
+bp_data <- read_excel("//Users//fabiothon//Desktop//Code//Bloodpressure//bp.xlsx")
+
+head(bp_data)
+# Data conversion ----- 
+bp_data$time <- as.POSIXct(bp_data$time)
+head(bp_data$time)
+
+# Plot -----
+ggplot(bp_data, aes(x = time)) +
+  geom_line(aes(y = systolic_pressure, color = "Systolic")) +
+  geom_point(aes(y = systolic_pressure, color = "Systolic")) +
+  geom_line(aes(y = diastolic_pressure, color = "Diastolic")) +
+  geom_point(aes(y = diastolic_pressure, color = "Diastolic")) +
+  geom_line(aes(y = pulse, color = "Heart rate")) +
+  geom_point(aes(y = pulse, color = "Heart rate"))+
+  scale_color_manual(values = c("Systolic" = "red", "Diastolic" = "blue", 
+                                "Heart rate" = "darkgrey")) +
+  labs(title = "Blood pressure of Human (2000)",
+       subtitle = paste("Average (Systolic):", round(mean(bp_data$systolic_pressure), 
+                                                     digits = 1 ), 
+                        " / Average (Diastolic):", round(mean(bp_data$diastolic_pressure), 
+                                                         digits = 1)),
+       caption = "System: Measurement DJ 239",
+       x = "Date", y = "Blood pressure [mmHg]", color = "Legend") +
+  theme_bw()  +
+  scale_y_continuous(sec.axis = sec_axis(~., name = "Heart rate [bpm]")) +
+  scale_x_datetime(breaks = seq(from = min(bp_data$time),
+                  to = max(bp_data$time),
+                  by = "24 hours"),
+                  labels = scales::date_format("%d.%m", tz = "CET"),
+                  expand = c(0.01, 0.01)
+                                          ) +
+  
+  #Adding labels ----
+
+  geom_text_repel(aes(y = systolic_pressure, label = systolic_pressure), 
+                color = "red", show.legend = FALSE, nudge_x = 0.1, nudge_y = 7,
+                segment.color = "grey") +
+  geom_text_repel(aes(y = diastolic_pressure, label = diastolic_pressure), 
+                  color = "blue", show.legend = FALSE, nudge_x = 0.1, nudge_y = 5, 
+                  segment.color = NA) +
+  geom_text_repel(aes(y = pulse, label = pulse), 
+                  color = "darkgrey", show.legend = FALSE, nudge_x = 0.1, nudge_y = 5,
+                  segment.color = NA)
+
+#Mean -----
+round(mean(bp_data$systolic_pressure), digits = 1)
+mean(bp_data$diastolic_pressure)
